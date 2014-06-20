dd83e6834f696b9ff6eaf9985f490465eed54f3f
Alojzij Blatnik <lojze.blatnik@gmail.com>
2014-06-20 21:29:59 +0200

    new hash dunno why

--- pkgs/tools/misc/stress/default.nix
+++ pkgs/tools/misc/stress/default.nix
@@ -5,7 +5,8 @@ stdenv.mkDerivation rec {
 
   src = fetchurl {
     url = "http://people.seas.harvard.edu/~apw/stress/stress-1.0.4.tar.gz";
-    sha256 = "f6642ae62ae0eb16368bce29181a15c7743129ea51c7184f45e161e21db3360b";
+    sha256 = "057e4fc2a7706411e1014bf172e4f94b63a12f18412378fca8684ca92408825b";
+    #sha256 = "f6642ae62ae0eb16368bce29181a15c7743129ea51c7184f45e161e21db3360b";
   };
 
   meta = {
