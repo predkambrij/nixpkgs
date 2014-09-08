8bebc04acad2ad8119c07989c698ba0d2193041e
Alojzij Blatnik <lojze.blatnik@gmail.com>
2014-09-08 19:44:51 +0200

    wacom tries
    Conflicts:
    	pkgs/os-specific/linux/xf86-input-wacom/default.nix

--- pkgs/development/libraries/libwacom/default.nix
+++ pkgs/development/libraries/libwacom/default.nix
@@ -1,11 +1,12 @@
 { fetchurl, stdenv, glib, pkgconfig, udev }:
 
 stdenv.mkDerivation rec {
-  name = "libwacom-0.7.1";
+  name = "libwacom-0.10";
 
   src = fetchurl {
     url = "mirror://sourceforge/linuxwacom/libwacom/${name}.tar.bz2";
-    sha256 = "1agdaa1bv5mp4l32qgsw63swnnv0p279jiy9madgw4y3d8d12dwm";
+    sha256 = "8eb714496bd340861b4a55a59744bce041156998700ee6d4c87dd5036e7eba00";
+    #sha256 = "1agdaa1bv5mp4l32qgsw63swnnv0p279jiy9madgw4y3d8d12dwm";
   };
 
   buildInputs = [ glib pkgconfig udev ];
