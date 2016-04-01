4b9000eedea6f72fb124e29dd076cc7c31981f8a
Alojzij Blatnik <lojze.blatnik@gmail.com>
2016-04-01 09:37:41 +0200

    upd btrfsProgs to 4.5.1

--- pkgs/tools/filesystems/btrfsprogs/default.nix
+++ pkgs/tools/filesystems/btrfsprogs/default.nix
@@ -1,28 +1,27 @@
-{ stdenv, fetchurl, attr, acl, zlib, libuuid, e2fsprogs, lzo
-, asciidoc, xmlto, docbook_xml_dtd_45, docbook_xsl, libxslt }:
+{ stdenv, fetchurl, pkgconfig, attr, acl, zlib, libuuid, e2fsprogs, lzo
+, asciidoc, xmlto, docbook_xml_dtd_45, docbook_xsl, libxslt
+}:
 
-#let version = "3.17.3"; in
-let version = "3.18.2"; in
+#let version = "4.2"; in
+let version = "4.5.1"; in
 
 stdenv.mkDerivation rec {
   name = "btrfs-progs-${version}";
 
   src = fetchurl {
     url = "mirror://kernel/linux/kernel/people/kdave/btrfs-progs/btrfs-progs-v${version}.tar.xz";
-    #sha256 = "0b5cvp8qk569r73003s2wisws6s8zlbmq9d1sdrmjlavv7w9zgg7";
-    #sha256 = "b23965d7214ebc93937df658f18fefe020fc2fd2be4d591094ef1aae168a36c8";
-    sha256 = "dd8ddb4d201e5ffd32a15ed0e1f33fbc2c8fb16383f1519a9fae136698c2dfec";
+    #sha256 = "1glglnglwz1rz6zy54xwdm601n5csbd1g73lx0rz8ay5jhz3q8r5";
+    sha256 = "eef87b2d0e231656afbffab35718c79a7774cc0101d33165abeb9bb2e017cefe";
   };
 
   buildInputs = [
-    attr acl zlib libuuid e2fsprogs lzo
+    pkgconfig attr acl zlib libuuid e2fsprogs lzo
     asciidoc xmlto docbook_xml_dtd_45 docbook_xsl libxslt
   ];
 
-  # for btrfs to get the rpath to libgcc_s, needed for pthread_cancel to work
-  NIX_CFLAGS_LINK = "-lgcc_s";
-
-  makeFlags = "prefix=$(out)";
+  # gcc bug with -O1 on ARM with gcc 4.8
+  # This should be fine on all platforms so apply universally
+  patchPhase = "sed -i s/-O1/-O2/ configure";
 
   meta = with stdenv.lib; {
     description = "Utilities for the btrfs filesystem";
