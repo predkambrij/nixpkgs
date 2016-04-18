4341a3ca029532c72f70e2683d27b8a1b2c48abe
Alojzij Blatnik <lojze.blatnik@gmail.com>
2016-04-18 10:48:08 +0200

    updating youtube-dl

--- pkgs/tools/misc/youtube-dl/default.nix
+++ pkgs/tools/misc/youtube-dl/default.nix
@@ -1,44 +1,46 @@
-{ stdenv, fetchgit, fetchurl, python, zip, pandoc, haskellPackages }:
-
-let
-  version = "2014.11.27";
-in
+{ stdenv, fetchgit, fetchurl, python, makeWrapper, ffmpeg, zip, haskellPackages
+, pandoc ? null
+}:
+# , buildPythonPackage, buildPythonApplication
 stdenv.mkDerivation rec {
-  name = "youtube-dl-${version}";
+  name = "youtube-dl-git";
+  #name = "youtube-dl-${version}";
+  #version = "2015.05.29";
 
   #src = fetchurl {
   #  url = "http://youtube-dl.org/downloads/${version}/${name}.tar.gz";
-  #  sha256 = "0bmjlp3jk5nd2i9jpbqbv2smh5rdxcjajv7fccfinmi6v2bjm1aa";
-  #src = fetchurl {
-  #  url = "http://youtube-dl.org/downloads/${version}/${name}.tar.gz";
-  #  sha256 = "1c9d5wjl0aaqfvz868wbkr3dwby9vjkx0hkbnwx0wnpg48nlnwpm";
+  #  sha256 = "0lgxir2i5ipplg57wk8gnbbsdrk7szqnyb1bxr97f3h0rbm4dfij";
   #};
   src = fetchgit {
     url = "https://github.com/rg3/youtube-dl.git";
-    rev = "1362bbbb4bfbd9380211a66032dbe3fe09aa2173"; #"fb17b60811ea89fb857ab03a997d193898046466";
+    rev = "e781ab63dba6968f54adb612907c76f6f9896095"; # 2016.03.01
   };
-  buildInputs = [ python haskellPackages.pandoc];
-  nativeBuildInputs = [ zip haskellPackages.pandoc];
 
-  ##buildInputs = [ python ];
-  ##nativeBuildInputs = [ zip pandoc ];
+  buildInputs = [ python makeWrapper zip pandoc ];
 
-    #sed -i 's/pandoc/#pandoc/g' Makefile
-  #patchPhase = ''
-  #  rm youtube-dl
-  #'';
+  #patchPhase = "rm youtube-dl";
 
   configurePhase = ''
     makeFlagsArray=( PREFIX=$out SYSCONFDIR=$out/etc PYTHON=${python}/bin/python )
   '';
 
-  meta = {
+  # Ensure ffmpeg is available in $PATH for post-processing & transcoding support.
+  postInstall = ''
+    wrapProgram $out/bin/youtube-dl --prefix PATH : "${ffmpeg}/bin"
+  '';
+
+  meta = with stdenv.lib; {
     homepage = "http://rg3.github.com/youtube-dl/";
     repositories.git = https://github.com/rg3/youtube-dl.git;
     description = "Command-line tool to download videos from YouTube.com and other sites";
-    license = stdenv.lib.licenses.unlicense;
-
-    platforms = with stdenv.lib.platforms; linux ++ darwin;
-    maintainers = with stdenv.lib.maintainers; [ bluescreen303 simons phreedom AndersonTorres ];
+    longDescription = ''
+      youtube-dl is a small, Python-based command-line program
+      to download videos from YouTube.com and a few more sites.
+      youtube-dl is released to the public domain, which means
+      you can modify it, redistribute it or use it however you like.
+    '';
+    license = licenses.publicDomain;
+    platforms = with platforms; linux ++ darwin;
+    maintainers = with maintainers; [ bluescreen303 simons phreedom AndersonTorres fuuzetsu ];
   };
 }
